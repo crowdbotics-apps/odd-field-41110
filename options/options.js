31b17a688bc4d522d1a4a13cd2440b9564048a87
Crowdbotics <team@crowdbotics.com>
2023-05-30 23:08:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "odd-field-41110",
+  url: "https://odd-field-41110.botics.co",
+  api: "https://odd-field-41110.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
